c13866c392cf2dbc064c095a51df41d8481f3ce8
albertleinsoo <albertleinsoo@mail.ru>
2023-08-28 12:07:37 +0300

    feat: stats-service client, stats-service server

--- stats-service/server/src/main/java/ru/practicum/StatsServiceApp.java
+++ stats-service/server/src/main/java/ru/practicum/StatsServiceApp.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainService {
+public class StatsServiceApp {
+
     public static void main(String[] args) {
-        SpringApplication.run(MainService.class, args);
+        SpringApplication.run(StatsServiceApp.class);
     }
 }
